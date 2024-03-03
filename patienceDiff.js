da18b0e5433923472003039b4bdcbde81a651f54
dminhhoang26 <75965698+dminhhoang26@users.noreply.github.com>
2024-03-03 20:27:05 +0700

    Update patienceDiff.js

--- patienceDiff.js
+++ patienceDiff.js
@@ -26,7 +26,6 @@
  */
 
 function patienceDiff(aLines, bLines, diffPlusFlag) {
-
   //
   // findUnique finds all unique values in arr[lo..hi], inclusive.  This
   // function is used in preparation for determining the longest common
@@ -38,45 +37,28 @@ function patienceDiff(aLines, bLines, diffPlusFlag) {
   //
 
   function findUnique(arr, lo, hi) {
-
     const lineMap = new Map();
-
     for (let i = lo; i <= hi; i++) {
-
       let line = arr[i];
-
       if (lineMap.has(line)) {
-
         lineMap.get(line).count++;
         lineMap.get(line).index = i;
-
       } else {
-
         lineMap.set(line, {
           count: 1,
           index: i
         });
-
       }
-
     }
 
     lineMap.forEach((val, key, map) => {
-
       if (val.count !== 1) {
-
         map.delete(key);
-
       } else {
-
         map.set(key, val.index);
-
       }
-
     });
-
     return lineMap;
-
   }
 
   //
@@ -89,31 +71,21 @@ function patienceDiff(aLines, bLines, diffPlusFlag) {
   // and bArray, with the Map value as an object containing the array indexes of
   // the matching unique lines.
   //
-
   function uniqueCommon(aArray, aLo, aHi, bArray, bLo, bHi) {
-
     const ma = findUnique(aArray, aLo, aHi);
     const mb = findUnique(bArray, bLo, bHi);
 
     ma.forEach((val, key, map) => {
-
       if (mb.has(key)) {
-
         map.set(key, {
           indexA: val,
           indexB: mb.get(key)
         });
-
       } else {
-
         map.delete(key);
-
       }
-
     });
-
     return ma;
-
   }
 
   //
@@ -123,58 +95,37 @@ function patienceDiff(aLines, bLines, diffPlusFlag) {
   // Returns an ordered array of objects containing the array indexes of the
   // matching lines for a LCS.
   //
-
   function longestCommonSubsequence(abMap) {
-
     const ja = [];
-
+    
     // First, walk the list creating the jagged array.
-
     abMap.forEach((val, key, map) => {
-
       let i = 0;
-
       while (ja[i] && ja[i][ja[i].length - 1].indexB < val.indexB) {
-
         i++;
-
       }
-
       if (!ja[i]) {
-
         ja[i] = [];
-
       }
 
       if (0 < i) {
-
         val.prev = ja[i - 1][ja[i - 1].length - 1];
-
       }
 
       ja[i].push(val);
-
     });
 
     // Now, pull out the longest common subsequence.
-
     let lcs = [];
-
     if (0 < ja.length) {
-
       let n = ja.length - 1;
       lcs = [ja[n][ja[n].length - 1]];
 
       while (lcs[lcs.length - 1].prev) {
-
         lcs.push(lcs[lcs.length - 1].prev);
-
       }
-
     }
-
     return lcs.reverse();
-
   }
 
   // "result" is the array used to accumulate the aLines that are deleted, the
@@ -200,19 +151,14 @@ function patienceDiff(aLines, bLines, diffPlusFlag) {
   //
 
   function addToResult(aIndex, bIndex) {
-
     if (bIndex < 0) {
-
       aMove.push(aLines[aIndex]);
       aMoveIndex.push(result.length);
       deleted++;
-
     } else if (aIndex < 0) {
-
       bMove.push(bLines[bIndex]);
       bMoveIndex.push(result.length);
       inserted++;
-
     }
 
     result.push({
@@ -220,7 +166,6 @@ function patienceDiff(aLines, bLines, diffPlusFlag) {
       aIndex: aIndex,
       bIndex: bIndex,
     });
-
   }
 
   //
@@ -228,15 +173,10 @@ function patienceDiff(aLines, bLines, diffPlusFlag) {
   // this function might recursively call recurseLCS to further match the lines
   // between aLines and bLines.
   //
-
   function addSubMatch(aLo, aHi, bLo, bHi) {
-
     // Match any lines at the beginning of aLines and bLines.
-
     while (aLo <= aHi && bLo <= bHi && aLines[aLo] === bLines[bLo]) {
-
       addToResult(aLo++, bLo++);
-
     }
 
     // Match any lines at the end of aLines and bLines, but don't place them
@@ -246,10 +186,8 @@ function patienceDiff(aLines, bLines, diffPlusFlag) {
     let aHiTemp = aHi;
 
     while (aLo <= aHi && bLo <= bHi && aLines[aHi] === bLines[bHi]) {
-
       aHi--;
       bHi--;
-
     }
 
     // Now, check to determine with the remaining lines in the subsequence
@@ -264,33 +202,21 @@ function patienceDiff(aLines, bLines, diffPlusFlag) {
     const uniqueCommonMap = uniqueCommon(aLines, aLo, aHi, bLines, bLo, bHi);
 
     if (uniqueCommonMap.size === 0) {
-
       while (aLo <= aHi) {
-
         addToResult(aLo++, - 1);
-
       }
 
       while (bLo <= bHi) {
-
         addToResult(- 1, bLo++);
-
       }
-
     } else {
-
       recurseLCS(aLo, aHi, bLo, bHi, uniqueCommonMap);
-
     }
 
     // Finally, let's add the matches at the end to the result.
-
     while (aHi < aHiTemp) {
-
       addToResult(++aHi, ++bHi);
-
     }
-
   }
 
   //
@@ -299,44 +225,30 @@ function patienceDiff(aLines, bLines, diffPlusFlag) {
   // recursively performs another LCS search (via addSubMatch), until there are
   // none found, at which point the subsequence is dumped to the result.
   //
-
   function recurseLCS(aLo, aHi, bLo, bHi, uniqueCommonMap) {
-
     const x = longestCommonSubsequence(uniqueCommonMap || uniqueCommon(aLines, aLo, aHi, bLines, bLo, bHi));
 
     if (x.length === 0) {
-
       addSubMatch(aLo, aHi, bLo, bHi);
-
     } else {
-
       if (aLo < x[0].indexA || bLo < x[0].indexB) {
-
         addSubMatch(aLo, x[0].indexA - 1, bLo, x[0].indexB - 1);
-
       }
 
       let i;
       for (i = 0; i < x.length - 1; i++) {
-
         addSubMatch(x[i].indexA, x[i + 1].indexA - 1, x[i].indexB, x[i + 1].indexB - 1);
-
       }
 
       if (x[i].indexA <= aHi || x[i].indexB <= bHi) {
-
         addSubMatch(x[i].indexA, aHi, x[i].indexB, bHi);
-
       }
-
     }
-
   }
 
   recurseLCS(0, aLines.length - 1, 0, bLines.length - 1);
 
   if (diffPlusFlag) {
-
     return {
       lines: result,
       lineCountDeleted: deleted,
@@ -347,7 +259,6 @@ function patienceDiff(aLines, bLines, diffPlusFlag) {
       bMove: bMove,
       bMoveIndex: bMoveIndex,
     };
-
   }
 
   return {
@@ -356,7 +267,6 @@ function patienceDiff(aLines, bLines, diffPlusFlag) {
     lineCountInserted: inserted,
     lineCountMoved: 0,
   };
-
 }
 
 /**
@@ -386,7 +296,6 @@ function patienceDiff(aLines, bLines, diffPlusFlag) {
  */
 
 function patienceDiffPlus(aLines, bLines) {
-
   const difference = patienceDiff(aLines, bLines, true);
 
   let aMoveNext = difference.aMove;
@@ -400,9 +309,7 @@ function patienceDiffPlus(aLines, bLines) {
   delete difference.bMoveIndex;
 
   let lastLineCountMoved;
-
   do {
-
     let aMove = aMoveNext;
     let aMoveIndex = aMoveIndexNext;
     let bMove = bMoveNext;
@@ -418,34 +325,24 @@ function patienceDiffPlus(aLines, bLines) {
     lastLineCountMoved = difference.lineCountMoved;
 
     subDiff.lines.forEach((v, i) => {
-
       if (0 <= v.aIndex && 0 <= v.bIndex) {
-
         difference.lines[aMoveIndex[v.aIndex]].moved = true;
         difference.lines[bMoveIndex[v.bIndex]].aIndex = aMoveIndex[v.aIndex];
         difference.lines[bMoveIndex[v.bIndex]].moved = true;
         difference.lineCountInserted--;
         difference.lineCountDeleted--;
         difference.lineCountMoved++;
-
       } else if (v.bIndex < 0) {
-
         aMoveNext.push(aMove[v.aIndex]);
         aMoveIndexNext.push(aMoveIndex[v.aIndex]);
-
       } else {
-
         bMoveNext.push(bMove[v.bIndex]);
         bMoveIndexNext.push(bMoveIndex[v.bIndex]);
-
       }
-
     });
-
   } while (0 < difference.lineCountMoved - lastLineCountMoved);
 
   return difference;
-
 }
 
 let a = 'Điểm đánh giá thường xuyên (1) - Học kỳ 1/ Single weight (1) - Semester 1'
